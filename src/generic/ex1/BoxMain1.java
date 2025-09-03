92089327ed3fe2b57e29f88308462eb15ad5d525
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-09-03 20:01:46 +0900

    feat/제네릭1_문제1_풀이

---
+++ src/generic/ex1/BoxMain1.java
@@ -0,0 +1,9 @@
+package generic.ex1;
+
+public class BoxMain1 {
+    public static void main(String[] args) {
+        IntegerBox integerBox = new IntegerBox();
+        integerBox.set(10);
+
+    }
+}
